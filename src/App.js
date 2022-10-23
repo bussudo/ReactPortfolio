3c649af39028d129b327b85cf425e32e271059f2
bussudo <105820926+bussudo@users.noreply.github.com>
2022-10-23 16:39:19 -0400

    have added basic structure

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PortfolioContainer from "./components/PortfolioContainer";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Alert, Breadcrumb, Card } from "react-bootstrap";
 
-function App() {
+const App = () => <PortfolioContainer />;
+
+function contactButton() {
   return (
-    <div className="App">
+    <div className="contactButton">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Card>
+          <Card.Img />
+          <Card.Body>
+            <Card.Title>Message</Card.Title>
+          </Card.Body>
+        </Card>
+        <Button>Submit</Button>
       </header>
     </div>
   );
---
+++ src/components/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <p>
+        Hi! I'm Donna, Web Designer and Developer from Oxford, Michigan. Please
+        take a look at my recent work and contact me for more information.
+      </p>
+    </div>
+  );
+}
