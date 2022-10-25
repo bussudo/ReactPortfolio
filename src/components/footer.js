7e14651272058343a417b95984b073a9f5d31a95
bussudo <105820926+bussudo@users.noreply.github.com>
2022-10-24 21:40:45 -0400

    fixing project page and footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,13 +1,24 @@
 import React from "react";
-import { Row, Container } from "react-bootstrap";
+import { Row, Container, Col } from "react-bootstrap";
+import { Link } from 'react-router-dom';
+import { url }
 
-export default function Footer() {
-  return;
-  <Container>
+export const openInNewTab = (url) => {
+    const newWindow = window.open(url, '_blank', 'noopener, noreferrer');
+    if (newWindow) newWindow.opener = null;
+};
+
+// export default function Footer() {
+    export const Footer = () => (
+  <Container fluid>
     <Row>
+    <Col md lg>
       <a href="https://github.com/bussudo">
         img src="/Assets/img/github.png" alt="github icon"
       </a>
+      </Col>
+      <Col>
+      </Col>
     </Row>
   </Container>;
-}
+    )
