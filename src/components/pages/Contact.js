a2943ee35ebd4c26b5108064b3ab72a55c7e9e59
bussudo <105820926+bussudo@users.noreply.github.com>
2022-10-28 19:33:51 -0400

    finishing up Contacts page

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,21 +1,35 @@
 import React from "react";
 import { Container, Form, Row, Button } from "react-bootstrap";
-import validator from "validator";
 
 // const getRegExp = (type) => {
 //   let regx = null;
 //   switch (type) {
 //     case 'EMAIL':
-//       regx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@(([[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+// let regx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@(([[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 //       break;
 //       default:
 //   }
 // }
-const validateEmail = (email) => {
-  return validator.isEmail(email) && email.length > 0;
-};
+
+// const validateEmail = (email) => {
+//   return validator.isEmail(email) && email.length > 0;
+// };
 
 export default function Contact() {
+  function handleField(event) {
+    console.log("away");
+    const nameInp = event.target.value.name;
+    const length = event.target.value.length;
+    var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+    if (length < 1) {
+      alert("Field cannot be blank");
+    }
+    if ((nameInp = "email" && !filter.test(email.value))) {
+      {
+        alert("Please provide a valid email address");
+      }
+    }
+  }
   return (
     <div>
       <h2>Contact Me!</h2>
@@ -24,13 +38,23 @@ export default function Contact() {
           <Row>
             <Form.Group className="mb-4" controlId="formName">
               <Form.Label>Name:</Form.Label>
-              <Form.Control type="input" />
+              <Form.Control
+                name="name"
+                onBlur={handleField}
+                type="input"
+                controlId="Name"
+              ></Form.Control>
             </Form.Group>
           </Row>
           <Row>
             <Form.Group className="mb-4" controlId="formEmail">
               <Form.Label>Email Address:</Form.Label>
-              <Form.Control type="email" placeholder="Example@email.com" />
+              <Form.Control
+                name="email"
+                onBlur={handleField}
+                type="email"
+                placeholder="Example@email.com"
+              />
               <Form.Text className="text-muted">
                 I won't share your email with anyone
               </Form.Text>
@@ -39,11 +63,20 @@ export default function Contact() {
           <Row>
             <Form.Group className="mb-4" controlId="formMessage">
               <Form.Label>Message</Form.Label>
-              <Form.Control as="textarea" rows={4} cols={50}></Form.Control>
+              <Form.Control
+                name="message"
+                onBlur={handleField}
+                as="textarea"
+                rows={4}
+                cols={50}
+                type="input"
+              ></Form.Control>
             </Form.Group>
           </Row>
           <Row>
-            <Button type="submit">Submit</Button>
+            <Button type="submit" value={"submit"}>
+              Submit
+            </Button>
           </Row>
         </Form>
       </Container>
--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -41,30 +41,23 @@ export default function Portfolio() {
           <Col md>
             <Card className="mb-3" style={{ color: "#000" }}>
               <Card.Body>
-                <a href="https://github.com/calihuddleston/park-me">
-                  <Card.Img src="./Assets/img/parkme.png" alt="car" />
+                <a href="https://github.com/bussudo/mvc.techblog">
+                  <Card.Img src="./Assets/img/mvctechblog.png" alt="techblog" />
                 </a>
-                <Card.Title>Park Me</Card.Title>
+                <Card.Title>Tech Blog</Card.Title>
               </Card.Body>
             </Card>
           </Col>
           <Col md>
             <Card className="mb-3" style={{ color: "#000" }}>
               <Card.Body>
-                <Card.Img src="./Assets/img/search.png" />
-
-                <a
-                  href="https://github.com/shannie14/We-Boot"
-                  alt="search by name or github"
-                ></a>
+                <Card.Img src="./Assets/img/PWATextEditor.png" />
                 <Row>
-                  <Card.Img src="./Assets/img/homepage.png" />
-
                   <a
-                    href="https://github.com/shannie14/We-Boot"
-                    alt="a list of github pages"
+                    href="https://github.com/bussudo/PWATextEditor"
+                    alt="text editor"
                   ></a>
-                  <Card.Title>SQL Employee Tracker</Card.Title>
+                  <Card.Title>PWA Text Editor</Card.Title>
                 </Row>
               </Card.Body>
             </Card>
@@ -84,9 +77,9 @@ export default function Portfolio() {
           <Col md>
             <Card className="mb-3" style={{ color: "#000" }}>
               <Card.Body>
-                <Card.Img src="./Assets/img/" />
+                <Card.Img src="./Assets/img/RMwireframe.png" />
                 <a
-                  href="https://github.com/Putridjungle602/Recycle-Me/"
+                  href="https://github.com/Putridjungle602/Recycle-Me/RMwireframe.png"
                   alt="recycling app"
                 ></a>
                 <Card.Title>Recycle Me</Card.Title>
