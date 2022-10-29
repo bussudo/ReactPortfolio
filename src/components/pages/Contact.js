6ac94ee5125a160f296ec558c59e55a7dbf64194
bussudo <105820926+bussudo@users.noreply.github.com>
2022-10-29 09:13:16 -0400

    made functionality changes for Contacts and Resume

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -4,68 +4,74 @@ import { Container, Form, Row, Button } from "react-bootstrap";
 export default function Contact() {
   function handleField(event) {
     console.log("away");
-    const nameInp = event.target.value.name;
+    // const nameInp = event.target.value.Text;
+    console.log(event.target.value);
 
     const length = event.target.value.length;
     const filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
     if (length < 1) {
       alert("Field cannot be blank");
     }
+    // if ((event.target.value.Text = "bussudo@gmail.com")) {
+    //   console.log("yes");
+    // }
+
     // if ((nameInp = "email" && (!filter.test(nameInp.value))
-    if ((nameInp = "email")) console.log(nameInp);
+    // if ((nameInp = "email")) console.log(nameInp);
     // console.log(filter.test(nameInp.value));
     // alert("Please provide a valid email address");
   }
+
+  return (
+    <div>
+      <h2>Contact Me!</h2>
+      <Container>
+        <Form>
+          <Row>
+            <Form.Group className="mb-4" controlId="formName">
+              <Form.Label>Name:</Form.Label>
+              <Form.Control
+                name="name"
+                onBlur={handleField}
+                type="input"
+                controlId="Name"
+              ></Form.Control>
+            </Form.Group>
+          </Row>
+          <Row>
+            <Form.Group className="mb-4" controlId="formEmail">
+              <Form.Label>Email Address:</Form.Label>
+              <Form.Control
+                name="email"
+                onBlur={handleField}
+                type="email"
+                placeholder="Example@email.com"
+              />
+              <Form.Text className="text-muted">
+                I won't share your email with anyone
+              </Form.Text>
+            </Form.Group>
+          </Row>
+          <Row>
+            <Form.Group className="mb-4" controlId="formMessage">
+              <Form.Label>Message</Form.Label>
+              <Form.Control
+                name="message"
+                onBlur={handleField}
+                as="textarea"
+                rows={4}
+                cols={50}
+                type="input"
+              ></Form.Control>
+            </Form.Group>
+          </Row>
+          <Row>
+            <Button type="submit" value={"submit"}>
+              Submit
+            </Button>
+          </Row>
+        </Form>
+      </Container>
+    </div>
+  );
 }
-return (
-  <div>
-    <h2>Contact Me!</h2>
-    <Container>
-      <Form>
-        <Row>
-          <Form.Group className="mb-4" controlId="formName">
-            <Form.Label>Name:</Form.Label>
-            <Form.Control
-              name="name"
-              onBlur={handleField}
-              type="input"
-              controlId="Name"
-            ></Form.Control>
-          </Form.Group>
-        </Row>
-        <Row>
-          <Form.Group className="mb-4" controlId="formEmail">
-            <Form.Label>Email Address:</Form.Label>
-            <Form.Control
-              name="email"
-              onBlur={handleField}
-              type="email"
-              placeholder="Example@email.com"
-            />
-            <Form.Text className="text-muted">
-              I won't share your email with anyone
-            </Form.Text>
-          </Form.Group>
-        </Row>
-        <Row>
-          <Form.Group className="mb-4" controlId="formMessage">
-            <Form.Label>Message</Form.Label>
-            <Form.Control
-              name="message"
-              onBlur={handleField}
-              as="textarea"
-              rows={4}
-              cols={50}
-              type="input"
-            ></Form.Control>
-          </Form.Group>
-        </Row>
-        <Row>
-          <Button type="submit" value={"submit"}>
-            Submit
-          </Button>
-        </Row>
-      </Form>
-    </Container>
-  </div>
-);
--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,5 +1,32 @@
 import React from "react";
+import { ListGroup } from "react-bootstrap";
 
 export default function Resume() {
-  return <h1>Resume goes here</h1>;
+  return <h1>Resume</h1>;
+  // <ListGroup<h2>
+  //   <ListGroup.Item>
+  <h2>
+    Download my
+    <a href="https://docdro.id/reuApNg" alt="resume download">
+      Resume
+    </a>
+  </h2>;
+  // </ListGroup.Item>
+  // </ListGroup>
+}
+
+{
+  function ActiveExample() {
+    return (
+      <ListGroup as="ul">
+        {/* <ListGroup.Item as="li" active> */}
+        <ListGroup.Item>Cras justo odio</ListGroup.Item>
+        <ListGroup.Item as="li">Dapibus ac facilisis in</ListGroup.Item>
+        <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+        <ListGroup.Item as="li">Porta ac consectetur ac</ListGroup.Item>
+      </ListGroup>
+    );
+  }
+  {
+  }
 }
